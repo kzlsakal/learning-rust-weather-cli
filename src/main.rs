35b10817fccc6ad15bbb478007a08227217aabb8
kzlsakal <dorukgurleyen@gmail.com>
2022-02-12 02:17:43 -0600

    Add waqi.info request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use reqwest::header::{USER_AGENT};
+
 fn main() {
-   println!("Hello, world!");
+  let api_token = std::env::var("API_TOKEN").expect("expected there to be an API token");
+
+  let mut arg_iterator = std::env::args();
+  arg_iterator.next();
+  let args: String = arg_iterator.collect();
+
+  let client = reqwest::blocking::Client::new();
+
+  let response = client
+    .get("https://api.waqi.info/search/")
+    .header(USER_AGENT, "weather_cli")
+    .query(&[("token", api_token), ("keyword", args)])
+    .send()
+    .expect("a successful request")
+    .json::<serde_json::Value>()
+    .expect("the body to be json");
+
+  dbg!(response);
 }
+
